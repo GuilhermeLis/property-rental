0d308c85b886a2a2aaa1787c0224a01a744c4043
GuilhermeLis <guilherme.lisboa.slv@gmail.com>
2020-12-02 10:19:56 -0300

    new seeFreeTime

---
+++ src/querys/seeFreeTime.js
@@ -0,0 +1,23 @@
+
+const database = require('./database')
+
+module.exports = async function() {
+  await database.connect()
+
+  const {rows} = await database.query(`
+    select
+        fs.id,
+        to_char(fs.schedule_date, 'Day, DD-MM-YYYY') as date,
+        p.city,
+        p.neighbornhood,
+        p.federative_unity,
+        p.country,
+        p.comment
+        from properties p
+      join free_schedule fs on p.id = fs.propertie_id
+      where fs.available = true;
+  `)
+
+  await database.end()
+  return rows;
+}
